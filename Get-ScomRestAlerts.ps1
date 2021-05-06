0973981d45ab63821e8742da55de5ec14b920648
Emre Güçlü <emreguclu@hotmail.com>
2021-05-06 12:05:36 +0300

    added Get-ScomRestAlerts.ps1

---
+++ Get-ScomRestAlerts.ps1
@@ -0,0 +1,202 @@
+<#
+.Synopsis
+   Gets scom rest alerts for exporting to third party systems.
+.DESCRIPTION
+   Gets scom rest alerts for exporting to third party systems.
+.EXAMPLE
+   .\Get-SCOMRestAlerts.ps1 -ManagementServer $env:COMPUTERNAME -Verbose
+    
+    VERBOSE: Authentication URL = http://SCOM/OperationsManager/authenticate
+    VERBOSE: Token from the webssion = SCOM-CSRF-TOKEN=5jtpzpbbqZ9vIkBLM893DR_u6LvWTC-vECiSqMyM3jsi1viQMcXXKgI96EeAn-eMqkmL-5xh-V5RKDdbDwSPhwcUrMf7etptw1emnWt5XwE1%3aN6u7vu4vmiLLjbvMRzpSe-1GE2JgD6yDFScj99-J
+    vm1q4AIYKMnUW2yOcs8hrTMaeKvq78Mtm5HseMv2sJxdlTnQbE6UW5uYxzTzwUajQFVBPE2_Y1KE2GhXYgJ_6fok0
+    VERBOSE: POST http://scom/OperationsManager/authenticate with -1-byte payload
+    VERBOSE: received 90-byte response of content type application/json; charset=utf-8
+    VERBOSE: Current authentication will last for 24 hours.
+    VERBOSE: POST http://scom/OperationsManager/data/alert with -1-byte payload
+    VERBOSE: received 4004-byte response of content type application/json; charset=utf-8
+    VERBOSE: 2 number of alerts returned.
+
+    id                          : 13d4ad97-8b60-40a3-b38e-934819111e96
+    severity                    : Error
+    monitoringobjectdisplayname : WS1
+    monitoringobjectpath        : web03.contoso.com
+    name                        : Could not connect to Log Analytics Workspace
+    age                         : 23 days, 21 hours
+    ageinmilliseconds           : 2064774165.8838
+    description                 : Could not connect to the following workspace.
+                                              WorkspaceID: 14943d3b-6e39-40e2-9bac-dd3d1195aeca
+                                              StatusText: DNS name resolution of the Microsoft Operations Management Suite service failed. This could be due to either the Workspace ID being configured 
+                                  incorrectly, or the agent not having Internet access. Please check that the system either has Internet access, or that a valid HTTP proxy has been configured for the 
+                                  agent.
+    owner                       : 
+    timeadded                   : 2021-04-12T11:03:44.2100000Z
+    repeatcount                 : 0
+    netbioscomputername         : web03
+    netbiosdomainname           : CONTOSO
+    ismonitoralert              : True
+
+    id                          : f308e471-a7e0-4dd8-bc28-a709ef6ee653
+    severity                    : Error
+    monitoringobjectdisplayname : web01.contoso.com
+    monitoringobjectpath        : Microsoft.SystemCenter.AgentWatchersGroup
+    name                        : Failed to Connect to Computer
+    age                         : 99 days, 6 hours
+    ageinmilliseconds           : 8575594818.8838005
+    description                 : The computer web01.contoso.com was not accessible.
+    owner                       : 
+    timeadded                   : 2021-01-27T03:30:03.6370000Z
+    repeatcount                 : 0
+    netbioscomputername         : 
+    netbiosdomainname           : 
+    ismonitoralert              : True
+
+
+#>
+
+<#
+
+
+PS C:\Source\scripts> .\Get-SCOMRestAlerts.ps1 -ManagementServer $env:COMPUTERNAME -Verbose
+VERBOSE: Authentication URL = http://SCOM/OperationsManager/authenticate
+VERBOSE: Token from the webssion = SCOM-CSRF-TOKEN=5jtpzpbbqZ9vIkBLM893DR_u6LvWTC-vECiSqMyM3jsi1viQMcXXKgI96EeAn-eMqkmL-5xh-V5RKDdbDwSPhwcUrMf7etptw1emnWt5XwE1%3aN6u7vu4vmiLLjbvMRzpSe-1GE2JgD6yDFScj99-J
+vm1q4AIYKMnUW2yOcs8hrTMaeKvq78Mtm5HseMv2sJxdlTnQbE6UW5uYxzTzwUajQFVBPE2_Y1KE2GhXYgJ_6fok0
+VERBOSE: POST http://scom/OperationsManager/authenticate with -1-byte payload
+VERBOSE: received 90-byte response of content type application/json; charset=utf-8
+VERBOSE: Current authentication will last for 24 hours.
+VERBOSE: POST http://scom/OperationsManager/data/alert with -1-byte payload
+VERBOSE: received 4004-byte response of content type application/json; charset=utf-8
+VERBOSE: 5 number of alerts returned.
+
+
+id                          : b1be5c73-6b9b-41c5-a07d-262101c271d3
+severity                    : Error
+monitoringobjectdisplayname : web01.contoso.com
+monitoringobjectpath        : Microsoft.SystemCenter.AgentWatchersGroup
+name                        : Health Service Heartbeat Failure
+age                         : 99 days, 6 hours
+ageinmilliseconds           : 8575607628.8838005
+description                 : The System Center Management Health Service on computer web01.contoso.com failed to heartbeat.
+owner                       : 
+timeadded                   : 2021-01-27T03:29:50.7200000Z
+repeatcount                 : 0
+netbioscomputername         : 
+netbiosdomainname           : 
+ismonitoralert              : True
+
+id                          : 140a3f6f-5117-492c-9303-3bf3b18dd2a6
+severity                    : Error
+monitoringobjectdisplayname : web02.contoso.com
+monitoringobjectpath        : Microsoft.SystemCenter.AgentWatchersGroup
+name                        : Health Service Heartbeat Failure
+age                         : 122 days, 18 hours
+ageinmilliseconds           : 10608965278.8838
+description                 : The System Center Management Health Service on computer web02.contoso.com failed to heartbeat.
+owner                       : 
+timeadded                   : 2021-01-03T14:40:32.9670000Z
+repeatcount                 : 2
+netbioscomputername         : 
+netbiosdomainname           : 
+ismonitoralert              : True
+
+id                          : 13d4ad97-8b60-40a3-b38e-934819111e96
+severity                    : Error
+monitoringobjectdisplayname : WS1
+monitoringobjectpath        : web03.contoso.com
+name                        : Could not connect to Log Analytics Workspace
+age                         : 23 days, 21 hours
+ageinmilliseconds           : 2064774165.8838
+description                 : Could not connect to the following workspace.
+                                          WorkspaceID: 14943d3b-6e39-40e2-9bac-dd3d1195aeca
+                                          StatusText: DNS name resolution of the Microsoft Operations Management Suite service failed. This could be due to either the Workspace ID being configured 
+                              incorrectly, or the agent not having Internet access. Please check that the system either has Internet access, or that a valid HTTP proxy has been configured for the 
+                              agent.
+owner                       : 
+timeadded                   : 2021-04-12T11:03:44.2100000Z
+repeatcount                 : 0
+netbioscomputername         : web03
+netbiosdomainname           : CONTOSO
+ismonitoralert              : True
+
+id                          : f308e471-a7e0-4dd8-bc28-a709ef6ee653
+severity                    : Error
+monitoringobjectdisplayname : web01.contoso.com
+monitoringobjectpath        : Microsoft.SystemCenter.AgentWatchersGroup
+name                        : Failed to Connect to Computer
+age                         : 99 days, 6 hours
+ageinmilliseconds           : 8575594818.8838005
+description                 : The computer web01.contoso.com was not accessible.
+owner                       : 
+timeadded                   : 2021-01-27T03:30:03.6370000Z
+repeatcount                 : 0
+netbioscomputername         : 
+netbiosdomainname           : 
+ismonitoralert              : True
+
+#>
+
+
+
+Param(
+
+    [Parameter(Mandatory = $true)]
+    [string]$ManagementServer,
+    [ValidateSet('New','Closed','All')]
+    $ResolutionState
+
+)
+
+
+
+# Set the Header and the Body
+$SCOMHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$SCOMHeaders.Add('Content-Type', 'application/json; charset=utf-8')
+$BodyRaw = "Windows"
+$Bytes = [System.Text.Encoding]::UTF8.GetBytes($BodyRaw)
+$EncodedText = [Convert]::ToBase64String($Bytes)
+$JSONBody = $EncodedText | ConvertTo-Json
+ 
+# The SCOM REST API authentication URL
+$URIBase = "http://$ManagementServer/OperationsManager/authenticate"
+Write-Verbose "Authentication URL = $URIBase"
+ 
+# Initiate the Cross-Site Request Forgery (CSRF) token, this is to prevent CSRF attacks
+$CSRFtoken = $WebSession.Cookies.GetCookies($URIBase) | ? { $_.Name -eq 'SCOM-CSRF-TOKEN' }
+Write-Verbose "Token from the webssion = $CSRFtoken"
+$SCOMHeaders.Add('SCOM-CSRF-TOKEN', [System.Web.HttpUtility]::UrlDecode($CSRFtoken.Value))
+
+try {
+# Authentication
+$Authentication = Invoke-RestMethod -Method Post -Uri $URIBase -Headers $SCOMHeaders -body $JSONBody -UseDefaultCredentials -SessionVariable WebSession -ErrorAction stop
+}
+Catch {
+
+throw "Could not authenticate, Exiting. Error: $($_.Exception.Message)"
+
+}
+$TokenLifeTimeHours = [Math]::Round((([datetime]::Parse( $Authentication.expiryTime))  - (Get-Date)).TotalHours,2)
+Write-Verbose "Current authentication will last for $TokenLifeTimeHours hours."
+# The query which contains the criteria for our alerts
+switch ($ResolutionState)
+{
+    'New' {$Criteria = "(ResolutionState = '0')"}
+    'Closed' {$Criteria = "(ResolutionState = '255')"}
+    'All' {$Criteria = "(ResolutionState = '0') or (ResolutionState = '255')"}
+    Default {$Criteria = "(ResolutionState = '0') or (ResolutionState = '255')"}
+}
+$Query = @(@{         
+        
+        'classId' = ''
+        # Get all alerts with severity '2' (critical) and resolutionstate '0' (new)
+        'criteria' = $Criteria
+        'displayColumns' ='severity','monitoringobjectdisplayname','monitoringobjectpath','name','age','description','owner','timeadded','repeatcount','netbioscomputername','netbiosdomainname','ismonitoralert','resolutionstate'
+})
+ 
+# Convert our query to JSON format
+$JSONQuery = $Query | ConvertTo-Json
+ 
+$Response = Invoke-RestMethod -Uri "http://$ManagementServer/OperationsManager/data/alert" -Method Post -Body $JSONQuery -ContentType "application/json" -WebSession $WebSession
+ 
+# Print out the alert results
+$Alerts = $Response.Rows
+Write-Verbose "$($Alerts.Count) number of alerts returned."
+$Alerts
